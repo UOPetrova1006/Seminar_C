7bd18d3b680844abb2394436d93f4a214282abc0
Uylia Petrova <cachorro1@ya.ru>
2022-08-15 15:48:04 +0300

    Seminar1 added

---
+++ Seminar1/Program.cs
@@ -0,0 +1,32 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите оператор +/-/*/: или end для выхода");
+    string Operator = Console.ReadLine();
+     if (Operator == "end")
+     {
+        break;
+     }
+    Console.WriteLine("Введите 1 число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите 2 число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    
+    if (Operator == "+")
+    {
+        Console.WriteLine(number1 + number2);
+    }
+    else if (Operator == "-")
+    {
+        Console.WriteLine(number1 - number2);
+    }
+    else if (Operator == "*")
+    {
+        Console.WriteLine(number1 * number2);
+    }
+    else if (Operator == ":")
+    {
+        Console.WriteLine(number1 / number2);
+    }
+}
+
+
